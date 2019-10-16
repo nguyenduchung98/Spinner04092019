5d912132fadb0e0c5e2e0afcbd720a38513b4c95
Nguyen Duc Hung <hungrom98x@gmail.com>
2019-10-16 20:21:20 +0700

    set up spinner

--- app/src/main/java/nguyenduchung/ndh/spinner04092019/MainActivity.java
+++ app/src/main/java/nguyenduchung/ndh/spinner04092019/MainActivity.java
@@ -3,12 +3,42 @@ package nguyenduchung.ndh.spinner04092019;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
+    Spinner mSpinner;
+    ArrayList<String> mArrayTen =new ArrayList<>();
+    ArrayAdapter mAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mSpinner = findViewById(R.id.spinner);
+        mArrayTen = new ArrayList<>(Arrays.asList("Teo","Ty","Suu","Rom"));
+
+        mAdapter = new ArrayAdapter(MainActivity.this,android.R.layout.simple_spinner_dropdown_item,mArrayTen);
+        mSpinner.setAdapter(mAdapter);
+
+        mSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(MainActivity.this, mArrayTen.get(position), Toast.LENGTH_SHORT).show();
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
     }
 }
